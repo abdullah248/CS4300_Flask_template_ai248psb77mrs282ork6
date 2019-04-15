f0022d5b4ba1c8b22e89e7da328cbb4ae198bb05
Abdullah Islam <ai.islam1433@gmail.com>
2019-04-15 12:18:14 -0400

    Updated website to add more views and updated searchcontroller to read from site

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,6 +8,24 @@ net_id = "Abdullah Islam: ai248, Prathamesh Bang: psb77, Milan Shah: mrs282, Oha
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+	arr = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+	count = 0
+	basestr = "select"
+	baserange = "range"
+	select = request.args.get(basestr+str(count))
+	range = request.args.get(baserange +str(count))
+	blah = request.args.get("askdfjaksdfjas;df")
+	while select!=None and range!=None:
+		arr[int(select)] = int(range)
+		count = count +1
+		select = request.args.get(basestr+str(count))
+		range = request.args.get(baserange +str(count))
+
+	if blah==None:
+		blah1 = str("worked")
+	# while(select)
+	q = request.args
+
 	if not query:
 		data = []
 		output_message = ''
@@ -15,5 +33,5 @@ def search():
 		view = {"Abortion":1,"Taxes":0}
 		data = [{"idx":1,"pic":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/440px-Donald_Trump_official_portrait.jpg","name":"Donald Trump","party":"Republican","views":view},
 				{"idx":2,"pic":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg/440px-Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg","name":"Hillary Clinton","party":"Democrat","views":view}]
-		output_message = "Your search: " + query
+		output_message = "Your search: "  + str(arr)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
