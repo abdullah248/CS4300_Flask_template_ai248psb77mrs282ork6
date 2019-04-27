94481659f683569ca20d2f351a13db8aef94884a
Milan Shah <mrs282@cornell.edu>
2019-04-26 23:56:24 -0400

    yeet

--- app/static/scripts/addquestion.js
+++ app/static/scripts/addquestion.js
@@ -8,33 +8,39 @@ function addQuestion()
     {
         if (count < limit)
         {
-            var di = document.createElement('div')
-            di.style.paddingLeft = "3px";
+            var di = document.createElement('div');
+            var di2 = document.createElement('div');
+            var di3 = document.createElement('div');
+            di2.setAttribute("class", "customSelect");
+            di3.style.padding = "2rem";
             var linebreak = document.createElement("br");
             var select = document.createElement('select');
+            select.id = "select" + count;
             select.name = "select" + count;
-            select.class = "select"
-            select.options[0] = new Option("Abortion is a woman's unrestricted right", "0");
-            select.options[1] = new Option("Legally require hiring women & minorities", "1");
-            select.options[2] = new Option("Comfortable with same-sex marriage", "2");
-            select.options[3] = new Option("Keep God in the public sphere", "3");
-            select.options[4] = new Option("Expand ObamaCare", "4");
-            select.options[5] = new Option("Privatize Social Security", "5");
-            select.options[6] = new Option("Vouchers for school choice", "6");
-            select.options[7] = new Option("Fight EPA regulatory over-reach", "7");
-            select.options[8] = new Option("Stricter punishment reduces crime", "8");
-            select.options[9] = new Option("Absolute right to gun ownership", "9");
-            select.options[10] = new Option("Higher taxes on the wealthy", "10");
-            select.options[11] = new Option("Pathway to citizenship for illegal aliens", "11");
-            select.options[12] = new Option("Support & expand free trade", "12");
-            select.options[13] = new Option("Support American Exceptionalism", "13");
-            select.options[14] = new Option("Expand the military", "14");
-            select.options[15] = new Option("Make voter registration easier", "15");
-            select.options[16] = new Option("Avoid foreign entanglements", "16");
-            select.options[17] = new Option("Prioritize green energy", "17");
-            select.options[18] = new Option("Marijuana is a gateway drug", "18");
-            select.options[19] = new Option("Stimulus better than market-led recovery", "19");
-            select.value = count.toString();
+            select.class = "select";
+            select.options[0] = new Option("--Please choose an issue--", "");
+            select.options[0].disabled = true;
+            select.options[1] = new Option("Abortion is a woman's unrestricted right", "0");
+            select.options[2] = new Option("Legally require hiring women & minorities", "1");
+            select.options[3] = new Option("Comfortable with same-sex marriage", "2");
+            select.options[4] = new Option("Keep God in the public sphere", "3");
+            select.options[5] = new Option("Expand ObamaCare", "4");
+            select.options[6] = new Option("Privatize Social Security", "5");
+            select.options[7] = new Option("Vouchers for school choice", "6");
+            select.options[8] = new Option("Fight EPA regulatory over-reach", "7");
+            select.options[9] = new Option("Stricter punishment reduces crime", "8");
+            select.options[10] = new Option("Absolute right to gun ownership", "9");
+            select.options[11] = new Option("Higher taxes on the wealthy", "10");
+            select.options[12] = new Option("Pathway to citizenship for illegal aliens", "11");
+            select.options[13] = new Option("Support & expand free trade", "12");
+            select.options[14] = new Option("Support American Exceptionalism", "13");
+            select.options[15] = new Option("Expand the military", "14");
+            select.options[16] = new Option("Make voter registration easier", "15");
+            select.options[17] = new Option("Avoid foreign entanglements", "16");
+            select.options[18] = new Option("Prioritize green energy", "17");
+            select.options[19] = new Option("Marijuana is a gateway drug", "18");
+            select.options[20] = new Option("Stimulus better than market-led recovery", "19");
+            select.value = "";
 
             var range = document.createElement('input');
             range.class = "range";
@@ -45,8 +51,6 @@ function addQuestion()
             range.min = '1';
             range.max = '5';
             range.step = '1';
-
-
 
             var label = document.createElement('p');
             label.id = 'label' + count;
@@ -82,8 +86,10 @@ function addQuestion()
                 var row = table.insertRow(-1)
                 var cell1 = row.insertCell(0)
                 var cell2 = row.insertCell(1)
-                cell1.appendChild(select);
-                di.appendChild(range)
+                di2.appendChild(select);
+                di3.append(di2);
+                cell1.appendChild(di3);
+                di.appendChild(range);
                 cell2.appendChild(di);
                 cell2.appendChild(label);
                 // Increment the count
--- app/static/scripts/expand.js
+++ app/static/scripts/expand.js
@@ -1,12 +1,20 @@
-function expand(id, id2) {
+function expand(id, id3) {
+    console.log("Expanding");
     var x = document.getElementById(id);
+    var y = document.getElementById(id+50);
     if (x.style.display === "none") {
         x.style.display = "table-cell";
-        id2.value = "-";
-        id2.style.backgroundColor = "#f04b4c";
+        x.style.borderTopRightRadius = "10px";
+        x.style.borderBottomRightRadius = "10px";
+        y.style.borderTopRightRadius = "0px";
+        y.style.borderBottomRightRadius = "0px";
+        id3.value = "-";
+        id3.style.backgroundColor = "#f04b4c";
     } else {
         x.style.display = "none";
-        id2.value = "+";
-        id2.style.backgroundColor = "#009688";
+        y.style.borderTopRightRadius = "10px";
+        y.style.borderBottomRightRadius = "10px";
+        id3.value = "+";
+        id3.style.backgroundColor = "#009688";
     }
 }
